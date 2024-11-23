e87de8d313eadcfc83ea5f76f2e573e82004c065
Teodoro Marques <166256943+teomarques@users.noreply.github.com>
2024-11-23 23:51:28 +0000

    1º commit Produto.java

--- Produto.java
+++ Produto.java
@@ -1,5 +1,5 @@
 /**
- * Classe Abstrata Produto
+ * Classe Abstrata Produto implementa taxavel
  * @author teomarques
  * @version 1.0
  */
@@ -10,6 +10,14 @@ public abstract class Produto implements Taxavel {
     protected int quantidade;
     protected double valorUnitario;
 
+    /**
+     * setter para todos os atributos de Produto
+     * @param codigo
+     * @param nome
+     * @param descricao
+     * @param quantidade
+     * @param valorUnitario
+     */
     public Produto(String codigo, String nome, String descricao, int quantidade, double valorUnitario) {
         this.codigo = codigo;
         this.nome = nome;
@@ -18,17 +26,33 @@ public abstract class Produto implements Taxavel {
         this.valorUnitario = valorUnitario;
     }
 
+    /**
+     * método para calcular valor total da compra
+     * @return
+     */
     public double calcularValorTotal() {
         return quantidade * valorUnitario;
     }
 
+    /**
+     * método abstrato implementa getTaxaIVA da interface Taxavel
+     * @return
+     */
     @Override
     public abstract double getTaxaIVA();
 
+    /**
+     * método abstrato implementa calcularValorComIVA da interface Taxavel
+     * @return
+     */
     @Override
     public double calcularValorComIVA() {
         return calcularValorTotal() * (1 + getTaxaIVA() / 100);
     }
 
+    /**
+     * Método getter getNome
+     * @return
+     */
     public String getNome() { return nome; }
 }
