950c43ba9d7cbdf3491b95a36254807e272035c6
Teodoro Marques <166256943+teomarques@users.noreply.github.com>
2024-11-23 23:54:25 +0000

    1º commit Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
+
 /**
- * Super-Classe Cliente
+ * Classe Cliente
  * @author teomarques
  * @version 1.0
  */
@@ -7,14 +9,60 @@ public class Cliente {
     private String nome;
     private String nif;
     private String localizacao;
+    private TipoTaxa taxaPadrao; // Conexão adicional com TipoTaxa
+    private ArrayList<Certificacao> certificacoesNecessarias; // Conexão adicional com Certificacao
 
-    public Cliente(String nome, String nif, String localizacao) {
+    /**
+     * método setter Cliente para atributos da classe Cliente
+     * @param nome
+     * @param nif
+     * @param localizacao
+     * @param taxaPadrao
+     */
+    public Cliente(String nome, String nif, String localizacao, TipoTaxa taxaPadrao) {
         this.nome = nome;
         this.nif = nif;
         this.localizacao = localizacao;
+        this.taxaPadrao = taxaPadrao;
+        this.certificacoesNecessarias = new ArrayList<>();
     }
 
+    /**
+     * método getter para Nome
+     * @return
+     */
     public String getNome() { return nome; }
+    /**
+     * método getter para NIF
+     * @return
+     */
     public String getNif() { return nif; }
+    /**
+     * método getter para Localizacao
+     * @return
+     */
     public String getLocalizacao() { return localizacao; }
+    /**
+     * método getter para TaxaPadrao
+     * @return
+     */
+    public TipoTaxa getTaxaPadrao() { return taxaPadrao; }
+    /**
+     * método getter para CertificacoesNecessarias
+     * @return
+     */
+    public ArrayList<Certificacao> getCertificacoesNecessarias() { return certificacoesNecessarias; }
+
+    /**
+     * Adiciona uma certificação ao cliente, caso ele ainda não a possua.
+     * @param certificacao Certificação a ser adicionada.
+     */
+    public void adicionarCertificacao(Certificacao certificacao) {
+        if (!certificacoesNecessarias.contains(certificacao)) {
+            certificacoesNecessarias.add(certificacao);
+            System.out.println("Certificação " + certificacao + " adicionada ao cliente " + nome);
+        } else {
+            System.out.println("O cliente já possui a certificação " + certificacao);
+        }
+    }
 }
